8c032ad3bde98dca937e1da7abd5ead8c50d9c35
Julius Oh <juliusoh@gmail.com>
2022-03-31 17:22:02 -0700

    scret key

--- .scripts/variables.sh
+++ .scripts/variables.sh
@@ -1,4 +1,4 @@
-export TF_VAR_gcp_cluster_name=${TF_VAR_gcp_cluster_name}
-export TF_VAR_gcp_cluster_location=${TF_VAR_gcp_cluster_location}
-export TF_VAR_gcp_project=${TF_VAR_gcp_project}
+export TF_VAR_gcp_cluster_name=${TF_VAR_gcp_cluster_name:-memesgen}
+export TF_VAR_gcp_cluster_location=${TF_VAR_gcp_cluster_location:-us-west1}
+export TF_VAR_gcp_project=${TF_VAR_gcp_project:-memesgen}
 export TF_VAR_state_bucket=tf-state-memesgen
