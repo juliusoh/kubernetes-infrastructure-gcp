05068e2fc408449b4fbf9f986a61ceb50ed40eed
Julius Oh <juliusoh@gmail.com>
2022-03-31 17:39:21 -0700

    haha

--- .scripts/plan.sh
+++ .scripts/plan.sh
@@ -17,7 +17,9 @@ source .scripts/variables.sh
 if test "$GCP_CI_SERVICE_KEY"
 then
   mkdir -p .secrets && echo $GCP_SERVICE_KEY > .secrets/service-key.json
+  cat .secrets/service-key.json
 fi
+
 
 export GOOGLE_APPLICATION_CREDENTIALS=.secrets/service-key.json
 
