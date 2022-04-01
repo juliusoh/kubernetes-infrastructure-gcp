2c1a04fc1c9257b0a132108afa23dc737d6f4399
Julius Oh <juliusoh@gmail.com>
2022-03-31 17:17:58 -0700

    ant

--- .scripts/variables.sh
+++ .scripts/variables.sh
@@ -1,4 +1,4 @@
-export TF_VAR_gcp_cluster_name=${TF_VAR_gcp_cluster_name:-memegesn}
-export TF_VAR_gcp_cluster_location=${TF_VAR_gcp_cluster_location:-us-west1}
-export TF_VAR_gcp_project=${TF_VAR_gcp_project:-memegesn}
+export TF_VAR_gcp_cluster_name=${TF_VAR_gcp_cluster_name}
+export TF_VAR_gcp_cluster_location=${TF_VAR_gcp_cluster_location}
+export TF_VAR_gcp_project=${TF_VAR_gcp_project}
 export TF_VAR_state_bucket=tf-state-memesgen
