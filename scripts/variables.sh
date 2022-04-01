58adbaf1a756722578852856680d7d4f4b94856d
Julius Oh <juliusoh@gmail.com>
2022-03-31 17:03:48 -0700

    lets test this

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,4 +1,4 @@
-export TF_VAR_gcp_cluster_name=${TF_VAR_gcp_cluster_name:-logicloud}
-export TF_VAR_gcp_cluster_location=${TF_VAR_gcp_cluster_location:-us-central1}
-export TF_VAR_gcp_project=${TF_VAR_gcp_project:-development-323604}
-export TF_VAR_state_bucket=tf-state-logicloud
+export TF_VAR_gcp_cluster_name=${TF_VAR_gcp_cluster_name:-memegesn}
+export TF_VAR_gcp_cluster_location=${TF_VAR_gcp_cluster_location:-us-west1}
+export TF_VAR_gcp_project=${TF_VAR_gcp_project:-memegesn}
+export TF_VAR_state_bucket=tf-state-memesgen
